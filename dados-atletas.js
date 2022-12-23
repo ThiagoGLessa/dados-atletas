23ae76707e5ae40b7c5cdd03b57f861a38c5f0fb
ThiagoGLessa <fulldencinhonho@gmail.com>
2022-12-23 02:42:31 -0300

    dados-atletas

--- dados-atletas.js
+++ dados-atletas.js
@@ -1,87 +1,87 @@
 class Atleta {
-    constructor (nome, idade, peso, altura, notas){
-      this.nome = nome
-      this.idade = idade
-      this.peso = peso
-      this.altura = altura
-      this.notas = notas
-  }
-  
-    calculaCategoria(){
-      if (this.idade >= 9 && this.idade <= 11){
-        return "Infantil"
-      }
-  
-      if (this.idade >= 12 && this.idade <= 13){
-        return "Juvenil"
-      }
-  
-      if (this.idade >= 14 && this.idade <= 15){
-        return "Intermediário"
-      }
-  
-      if (this.idade >= 16 && this.idade <= 30){
-        return "Adulto"
-      }
-  
-      else {
-        return "Sem categoria"
-      }
+    constructor(nome, idade, peso, altura, notas) {
+        this.nome = nome
+        this.idade = idade
+        this.peso = peso
+        this.altura = altura
+        this.notas = notas
     }
-  
-    calculaIMC(){
-      return this.peso / (this.altura * this.altura)
+
+    calculaCategoria() {
+        if (this.idade >= 9 && this.idade <= 11) {
+            return "Infantil"
+        }
+
+        if (this.idade >= 12 && this.idade <= 13) {
+            return "Juvenil"
+        }
+
+        if (this.idade >= 14 && this.idade <= 15) {
+            return "Intermediário"
+        }
+
+        if (this.idade >= 16 && this.idade <= 30) {
+            return "Adulto"
+        }
+
+        else {
+            return "Sem categoria"
+        }
     }
-    
-    calculaMediaValida(){
-      let notas = this.notas
-      let notasOrdenadas = notas.sort(function (a, b) { return a - b })
-      let notasComputadas = notasOrdenadas.slice(1, -1)
-      let soma = notasComputadas[0] + notasComputadas[1] + notasComputadas[2]
-        
+
+    calculaIMC() {
+        return this.peso / (this.altura * this.altura)
+    }
+
+    calculaMediaValida() {
+        let notas = this.notas
+        let notasOrdenadas = notas.sort(function (a, b) { return a - b })
+        let notasComputadas = notasOrdenadas.slice(1, -1)
+        let soma = notasComputadas[0] + notasComputadas[1] + notasComputadas[2]
+
         return soma / notasComputadas.length
-    
+
     }
-  
-    obtemNomeAtleta(){
-      return `Nome: ${this.nome}`
+
+    obtemNomeAtleta() {
+        return `Nome: ${this.nome}`
     }
-  
-    obtemIdadeAtleta(){
-      return `Idade: ${this.idade}`
+
+    obtemIdadeAtleta() {
+        return `Idade: ${this.idade}`
     }
-  
-    obtemPesoAtleta(){
-      return `Peso: ${this.peso}`
+
+    obtemPesoAtleta() {
+        return `Peso: ${this.peso}`
     }
-  
-    obtemAlturaAtleta(){
-      return `Altura: ${this.altura}`
+
+    obtemAlturaAtleta() {
+        return `Altura: ${this.altura}`
     }
-  
-    obtemNotasAtleta(){
-      return `Notas: ${this.notas}`
+
+    obtemNotasAtleta() {
+        return `Notas: ${this.notas}`
     }
-  
-    obtemCategoria(){
-      return `Categoria: ${atleta.calculaCategoria()}`
+
+    obtemCategoria() {
+        return `Categoria: ${atleta.calculaCategoria()}`
     }
-  
-    obtemIMC(){
-      return `IMC: ${atleta.calculaIMC()}`
+
+    obtemIMC() {
+        return `IMC: ${atleta.calculaIMC()}`
     }
-  
-    obtemMediaValida(){
-      return `Média válida: ${atleta.calculaMediaValida()}`
+
+    obtemMediaValida() {
+        return `Média válida: ${atleta.calculaMediaValida()}`
     }
-  }
-  
-  const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
-  console.log(`${atleta.obtemNomeAtleta()}
-  ${atleta.obtemIdadeAtleta()}
-  ${atleta.obtemPesoAtleta()}
-  ${atleta.obtemAlturaAtleta()}
-  ${atleta.obtemNotasAtleta()}
-  ${atleta.obtemCategoria()}
-  ${atleta.obtemIMC()}
-  ${atleta.obtemMediaValida()}`)
+}
+
+const atleta = new Atleta("Cesar Abascal", 30, 80, 1.70, [10, 9.34, 8.42, 10, 7.88])
+console.log(`${atleta.obtemNomeAtleta()}
+${atleta.obtemIdadeAtleta()}
+${atleta.obtemPesoAtleta()}
+${atleta.obtemAlturaAtleta()}
+${atleta.obtemNotasAtleta()}
+${atleta.obtemCategoria()}
+${atleta.obtemIMC()}
+${atleta.obtemMediaValida()}`)
